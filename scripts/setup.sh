bd43fca8248710aed0a14087f76b9cca10e09f34
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-07-02 14:30:06 +0200

    Sleep even longer.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,7 @@ MONGODB1=`ping -c 1 mongo1 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 MONGODB2=`ping -c 1 mongo2 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 MONGODB3=`ping -c 1 mongo3 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 
-sleep 10
+sleep 20
 
 mongo --host ${MONGODB1}:27017 <<EOF
    var cfg = {

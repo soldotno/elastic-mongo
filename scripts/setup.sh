d7f2f39004fde406c3b84a99401b69e83178574f
sisve <sinsvend@gmail.com>
2015-07-02 12:05:20 +0200

    Mongo Replicaset setup complete. (working)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-mongo --host mongo1:27017 <<EOF
-var cfg = {
-    "_id": "rs",
-    "version": 1,
-    "members": [
-        {
-            "_id": 0,
-            "host": "mongo1:27017",
-            "priority": 1
-        },
-        {
-            "_id": 1,
-            "host": "mongo2:27017",
-            "priority": 0
-        },
-        {
-            "_id": 2,
-            "host": "mongo3:27017",
-            "priority": 0
-        }
-    ]
-};
-rs.initiate(cfg);
-rs.reconfig(cfg, {force: true});
+MONGODB1=`ping -c 1 mongo1 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
+MONGODB2=`ping -c 1 mongo2 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
+MONGODB3=`ping -c 1 mongo3 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
+
+sleep 10
+
+mongo --host ${MONGODB1}:27017 <<EOF
+   var cfg = {
+        "_id": "rs",
+        "version": 1,
+        "members": [
+            {
+                "_id": 0,
+                "host": "${MONGODB1}:27017",
+                "priority": 2
+            },
+            {
+                "_id": 1,
+                "host": "${MONGODB2}:27017",
+                "priority": 1
+            },
+            {
+                "_id": 2,
+                "host": "${MONGODB3}:27017",
+                "priority": 1
+            }
+        ]
+    };
+    rs.initiate(cfg);
 EOF

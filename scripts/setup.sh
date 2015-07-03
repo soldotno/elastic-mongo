0e0e5307ab1cca8a0f01cb36678c561ab796a66d
sisve <sinsvend@gmail.com>
2015-07-03 13:21:17 +0200

    fixed conflict

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,11 +4,7 @@ MONGODB1=`ping -c 1 mongo1 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 MONGODB2=`ping -c 1 mongo2 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 MONGODB3=`ping -c 1 mongo3 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 
-<<<<<<< HEAD
-sleep 10
-=======
 sleep 20
->>>>>>> b187a2489e6205d70b4990d3d2edcd2d4929c845
 
 mongo --host ${MONGODB1}:27017 <<EOF
    var cfg = {
@@ -33,8 +29,4 @@ mongo --host ${MONGODB1}:27017 <<EOF
         ]
     };
     rs.initiate(cfg);
-<<<<<<< HEAD
 EOF
-=======
-EOF
->>>>>>> b187a2489e6205d70b4990d3d2edcd2d4929c845

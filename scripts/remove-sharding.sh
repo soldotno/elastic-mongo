876b81723a856b6872c7e5e68e7ed47dcb179386
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-07-10 20:03:36 +0200

    Wait for ES to start

--- scripts/remove-sharding.sh
+++ scripts/remove-sharding.sh
@@ -2,10 +2,21 @@
 
 ES=`ping -c 1 elasticsearch | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 
+echo "Waiting for startup.."
+echo  curl ${ES}:9200 2>&1 | grep "You Know, for Search"
+until curl ${ES}:9200 2>&1 | grep "You Know, for Search"; do
+  echo '.'
+  curl ${ES}:9200 2>&1
+  sleep 1
+done
+
+echo "Done waiting!"
+
 curl -XPUT ${ES}:9200/_cluster/settings -d '{
     "transient" : {
         "cluster.routing.allocation.disk.threshold_enabled" : false
     }
 }'
 
-sleep 86400
+sleep 500
+

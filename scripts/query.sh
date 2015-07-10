2120bccad08347810102c16d7a3e6c0dfd46dc9a
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-07-10 10:16:52 +0200

    Replace sleep with a while.

--- scripts/query.sh
+++ scripts/query.sh
@@ -5,10 +5,14 @@ MONGODB2=`ping -c 1 mongo2 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 MONGODB3=`ping -c 1 mongo3 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 ES=`ping -c 1 elasticsearch | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 
+echo "Waiting for the mongos to complete the election."
+until curl http://${MONGODB1}:28017/isMaster\?text\=1  2>&1 | grep ismaster | grep true; do
+  printf '.'
+  sleep 1
+done
+echo "The primary is elected."
 
-#MONGODB1=dockerhost
-#ES=dockerhost
-sleep 60
+
 echo QUERY – time now: `date +"%T" `
 
 echo "================================="

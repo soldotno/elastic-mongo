8fee9751ca7d39fea3d5fff27e8f3626850eec3f
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-07-16 10:39:55 +0200

    No need to install python when using python:latest

--- scripts/mongo-connector.sh
+++ scripts/mongo-connector.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 MONGODB1=`ping -c 1 mongo1 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
-MONGODB2=`ping -c 1 mongo2 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
-MONGODB3=`ping -c 1 mongo3 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 ELASTICSEARCH=`ping -c 1 elasticsearch | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 
 echo "Waiting for MongoDB to start.."
@@ -12,8 +10,9 @@ until curl http://${MONGODB1}:28017/serverStatus\?text\=1 2>&1 | grep uptime | h
 done
 echo "MongoDB has started!"
 
-
-apt-get update && apt-get install -y python-pip
+echo "Installing mongo-connector"
 pip install mongo-connector
 
-mongo-connector --auto-commit-interval=0 -m ${MONGODB1}:27017 -t ${ELASTICSEARCH}:9200 -d elastic_doc_manager  --stdout
+echo "Starting mongo-connector"
+mongo-connector --auto-commit-interval=5 -m ${MONGODB1}:27017 -t ${ELASTICSEARCH}:9200 -d elastic_doc_manager --stdout
+# ^ Add -v for more verbos logging.

ee33c33240d70ea3b2836733ded46a32023c9e23
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-08-03 09:11:13 +0200

    install mongo-connector first

--- scripts/mongo-connector.sh
+++ scripts/mongo-connector.sh
@@ -3,16 +3,16 @@
 MONGODB1=`ping -c 1 mongo1 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 ELASTICSEARCH=`ping -c 1 elasticsearch | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 
-echo "Waiting for MongoDB to start.."
-until curl http://${MONGODB1}:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
+
+pip install mongo-connector
+touch /scripts/mongo-connector-installed
+
+printf "\n\nWaiting for MongoDB to start\n"
+until curl http://${MONGO_URL}:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
   printf '.'
   sleep 1
 done
 echo "MongoDB has started!"
 
-pip install mongo-connector
-
 echo "\n\nStarting mongo-connector.."
-touch /scripts/mongo-connector-installed
-mongo-connector --auto-commit-interval=5 -m ${MONGODB1}:27017 -t ${ELASTICSEARCH}:9200 -d elastic_doc_manager --stdout
-# ^ Add -v for more verbos logging.
+mongo-connector --auto-commit-interval=5 -m ${MONGO_URL}:27017 -t ${ELASTICSEARCH_URL} -v -d elastic_doc_manager --stdout

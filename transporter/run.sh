283d9b8fce4330b34ac644539285ee13d0c8ad41
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-08-03 15:50:02 +0200

    This installs and starts transporter.

---
+++ transporter/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+MONGO=`ping -c 1 mongo1 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
+ES=`ping -c 1 elasticsearch | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
+
+
+# This script should only start transporter. The installation should be done in an image.
+
+cd $GOPATH; mkdir pkg
+mkdir -p src/github.com/compose; cd src/github.com/compose
+git clone https://github.com/compose/transporter; cd transporter
+echo "Getting dependencies and building.."
+go get -a ./cmd/...
+go build -a ./cmd/...
+
+/scripts/wait-until-started.sh
+
+cd /transporter
+transporter run --config ./config.yaml ./mongo-es.js

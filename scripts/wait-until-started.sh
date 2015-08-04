1ffae2b7809327f9c3600b85194de242b38905bc
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-08-04 09:43:36 +0200

    s/echo/printf/

--- scripts/wait-until-started.sh
+++ scripts/wait-until-started.sh
@@ -5,7 +5,7 @@ ES=`ping -c 1 elasticsearch | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 
 echo "Waiting for the mongos to complete the election."
 until curl http://${MONGODB1}:28017/isMaster\?text\=1  2>&1 | grep ismaster | grep true; do
-  echo '.'
+  printf '.'
   sleep 1
 done
 echo "The primary is elected."

97d16ab538bd75fd953cc8bcf83711b572561739
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-08-04 08:51:19 +0200

    Added tail:true to mongo source.

--- transporter/mongo-es.js
+++ transporter/mongo-es.js
@@ -1,5 +1,3 @@
-pipeline = Source({name:"mongo"})
+pipeline = Source({name:"mongo, tail:true"})
   .transform({filename: "transformers/passthrough_and_log.js", namespace: "harvester.entries"})
   .save({name:"es", namespace: "harvester.entries"})
-
-

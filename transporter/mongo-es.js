51a44201cc03c913ca7fee3db1d76f50551092f8
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-08-04 09:44:30 +0200

    fix typo and add namespace.

--- transporter/mongo-es.js
+++ transporter/mongo-es.js
@@ -1,3 +1,5 @@
-pipeline = Source({name:"mongo, tail:true"})
+pipeline = Source({name:"mongo", tail: true, namespace: "harvester.entries"})
   .transform({filename: "transformers/passthrough_and_log.js", namespace: "harvester.entries"})
   .save({name:"es", namespace: "harvester.entries"})
+
+

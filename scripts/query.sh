f7ed8de403d310b6f82b344e082bdd7cbf193a0d
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-08-06 14:25:00 +0200

    use harvester-test

--- scripts/query.sh
+++ scripts/query.sh
@@ -12,7 +12,7 @@ ES=`ping -c 1 elasticsearch | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 echo "================================="
 echo "Writing to MongoDB"
 mongo ${MONGODB1} <<EOF
-  use harvester
+  use harvester-test
   rs.config()
   var p = {title: "Breaking news", content: "It's not summer yet."}
   db.entries.save(p)
@@ -21,14 +21,14 @@ EOF
 
 echo "================================="
 echo "Fetching data from Mongo"
-echo curl http://${MONGODB1}:28017/harvester/entries/?limit=10
-curl http://${MONGODB1}:28017/harvester/entries/?limit=10
+echo curl http://${MONGODB1}:28017/harvester-test/entries/?limit=10
+curl http://${MONGODB1}:28017/harvester-test/entries/?limit=10
 echo "================================="
 
 
 printf "\nReading from Elasticsearch (waiting for the transporter to start)\n\n"
 sleep 40
-curl -XGET "http://${ES}:9200/harvester/_search?pretty&q=*:*"
+curl -XGET "http://${ES}:9200/harvester-test/_search?pretty&q=*:*"
 
 
 echo "================================="

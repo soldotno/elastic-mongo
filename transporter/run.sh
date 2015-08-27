70384874870866d16149b7c18a1dce8fcbe1808f
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-08-27 16:18:43 +0200

    old method for build does not work any more.

--- transporter/run.sh
+++ transporter/run.sh
@@ -9,10 +9,14 @@ ES=`ping -c 1 elasticsearch | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 cd $GOPATH; mkdir pkg
 mkdir -p src/github.com/compose; cd src/github.com/compose
 git clone https://github.com/compose/transporter; cd transporter
+git checkout tags/v0.1.0
 
 echo "Getting dependencies and building.."
-go get -a ./cmd/...
-go build -a ./cmd/...
+go get github.com/tools/godep
+godep restore
+godep go build ./cmd/...
+godep go install ./cmd/...
+
 
 /scripts/wait-until-started.sh
 

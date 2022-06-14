36eb4dc637908e761c8e0db876ab0196fec41c1a
Martin Stabenfeldt <martin@stabenfeldt.net>
2022-06-14 15:41:34 +0200

    New version of Transporter requires new config

---
+++ transporter/pipeline.js
@@ -0,0 +1,27 @@
+var source = mongodb({
+  uri: "mongodb://mongo1/harvester-test",
+  // "timeout": "30s",
+  // "tail": false,
+  // "ssl": false,
+  // "cacerts": ["/path/to/cert.pem"],
+  // "wc": 1,
+  // "fsync": false,
+  // "bulk": false,
+  // "collection_filters": "{}",
+  // "read_preference": "Primary"
+});
+
+var sink = elasticsearch({
+  uri: "http://elasticsearch:9200/",
+  // "timeout": "10s", // defaults to 30s
+  // "aws_access_key": "ABCDEF", // used for signing requests to AWS Elasticsearch service
+  // "aws_access_secret": "ABCDEF" // used for signing requests to AWS Elasticsearch service
+});
+
+//t.Source(source).Save(sink);
+//t.Source("source", source).Save("sink", sink)
+t.Source("source", source, "harvester-test.entries").Save(
+  "sink",
+  sink,
+  "harvester-test.entries"
+);
